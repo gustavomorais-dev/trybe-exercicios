93cd9e6a612600206ceedf50c83e771af2dcb778
gustavomorais <gustavomorais.dev@gmail.com>
2022-11-26 09:46:12 -0300

    corrige formatação

--- fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercicios-arrays.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercicios-arrays.js
@@ -2,14 +2,14 @@
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-/* Percorra o array imprimindo todos os valores nele contidos com a função console.log(); */
+/* 1 - Percorra o array imprimindo todos os valores nele contidos com a função console.log(); */
 console.log("_________________ Exercício 1 _________________")
 
 for (let number of numbers) {
     console.log(number);
 }
 
-/* Some todos os valores contidos no array e imprima o resultado; */
+/* 2 - Some todos os valores contidos no array e imprima o resultado; */
 console.log("_________________ Exercício 2 _________________")
 
 let soma1 = 0;
