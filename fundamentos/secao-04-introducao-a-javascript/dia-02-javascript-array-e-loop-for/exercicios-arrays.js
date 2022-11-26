2248a83eac4e62e18b16473de57a64010f40ca4e
gustavomorais <gustavomorais.dev@gmail.com>
2022-11-26 09:44:33 -0300

    exercicio 1

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercicios-arrays.js
@@ -0,0 +1,10 @@
+/* Iremos utilizar esse array para realizar os próximos exercícios. */
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+/* Percorra o array imprimindo todos os valores nele contidos com a função console.log(); */
+console.log("_________________ Exercício 1 _________________")
+
+for (let number of numbers) {
+    console.log(number);
+}
