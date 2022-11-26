379292b37caf905c43424126486b7b3bea4eca8b
gustavomorais <gustavomorais.dev@gmail.com>
2022-11-26 10:42:58 -0300

    adiciona exercicio bonus 1

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercicio-bonus-1.js
@@ -0,0 +1,15 @@
+/* Ordene o array numbers em ordem crescente e imprima seus valores; */
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let index = 0; index < numbers.length; index += 1) {
+    for (let index = 0; index < numbers.length; index += 1) {
+        if (numbers[index] > numbers[index + 1]) {
+            let currentArrayValue = numbers[index];
+            numbers[index] = numbers[index + 1];
+            numbers[index + 1] = currentArrayValue;
+        }
+    }
+}
+
+console.log(numbers);
