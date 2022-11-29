7cc41362af3a30581f9fb3854ae7e296dd87ccfc
gustavomorais <gustavomorais.dev@gmail.com>
2022-11-29 19:18:24 -0300

    adiciona exercicio bonus 3

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicio-bonus-3.js
@@ -0,0 +1,27 @@
+/* Agora inverta o lado do triângulo. Por exemplo:
+
+n = 5
+
+    *
+   **
+  ***
+ ****
+*****
+
+*/
+
+const n = 5;
+let line = '';
+
+for (let indexLine = 1; indexLine <= n; indexLine += 1) {
+    for (let indexColumn = 1; indexColumn <= indexLine; indexColumn += 1) {
+        for (let indexSpace = 1; indexSpace <= n - indexLine; indexSpace += 1) {
+            if (!line.includes('*')) {
+            line += ' '
+            }
+        }
+        line += '*';
+    }
+    console.log(line);
+    line = '';
+}
