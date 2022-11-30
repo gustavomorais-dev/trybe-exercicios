4233c3acffbbcedf59522f190f586722db766f23
gustavomorais <gustavomorais.dev@gmail.com>
2022-11-30 16:27:49 -0300

    adiciona ao exercicios-objetos-e-for-in.js a resolução do exercicio 4

--- fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/exercicios-objetos-e-for-in.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/exercicios-objetos-e-for-in.js
@@ -43,3 +43,16 @@ for (let key in info) {
 }
 
 //Fim
+console.log('______________________________________ Exercício 4 ______________________________________');
+/* Faça um novo for/in, mas agora mostre todos os valores das chaves do objeto. Valor esperado no console:
+Margarida
+Pato Donald
+Namorada do personagem principal nos quadrinhos do Pato Donald
+Sim
+*/
+
+for (let key in info) {
+    console.log(info[key]);
+}
+
+//Fim
