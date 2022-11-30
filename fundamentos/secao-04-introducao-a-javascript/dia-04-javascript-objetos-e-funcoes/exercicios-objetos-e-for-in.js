9fc699700dd8780204ee717282bbd26ce0d04859
gustavomorais <gustavomorais.dev@gmail.com>
2022-11-30 16:17:34 -0300

    adiciona ao exercicios-objetos-e-for-in.js a resolução do exercicio 2

--- fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/exercicios-objetos-e-for-in.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/exercicios-objetos-e-for-in.js
@@ -4,10 +4,29 @@ let info = {
     personagem: 'Margarida',
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-  };
+};
+//Resoluções
 
-  /* Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome. Use a sintaxe meuObjeto.chave. Valor esperado no console:
+console.log('______________________________________ Exercício 1 ______________________________________');
+/* Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome. Use a sintaxe meuObjeto.chave. Valor esperado no console:
 
-  Bem-vinda, Margarida */
+Bem-vinda, Margarida 
+*/
 
-  console.log('Bem vinda,', info.personagem);
+console.log('Bem vinda,', info.personagem);
+
+// Fim
+console.log('______________________________________ Exercício 2 ______________________________________');
+/* Insira no objeto uma nova propriedade com o nome de chave ‘recorrente’ e o valor ‘Sim’ e, em seguida, imprima o objeto no console. Use a sintaxe meuObjeto['chave'] = valor. Valor esperado no console: 
+{
+personagem: 'Margarida',
+origem: 'Pato Donald',
+nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+recorrente: 'Sim'
+} 
+*/
+
+info['recorrente'] = 'Sim';
+console.log(info);
+
+// Fim
