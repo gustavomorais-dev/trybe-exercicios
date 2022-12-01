81fa43b01cd1b41c747b84cbf2ad4cbe16b701b6
gustavomorais <gustavomorais.dev@gmail.com>
2022-11-30 22:32:47 -0300

    adiciona a resolução do exercicio-funcoes-6.js

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/exercicio-funcoes-6.js
@@ -0,0 +1,15 @@
+/* Crie uma função que receba um número natural (número inteiro não negativo) N e retorne o somatório de todos os números de 1 até N.
+
+Valor de teste: N = 5.
+
+Valor esperado no retorno da função: 1+2+3+4+5 = 15. */
+
+function sumUpTo(number) {
+    let sum = 0;
+    for (let index = 1; index <= number; index += 1) {
+        sum += index;
+    }
+    return sum;
+}
+
+console.log(sumUpTo(5));
