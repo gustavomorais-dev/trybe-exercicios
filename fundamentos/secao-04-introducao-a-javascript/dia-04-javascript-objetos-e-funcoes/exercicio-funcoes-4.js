5707ed044ae3665198bbedb001a388c11d91bf16
gustavomorais <gustavomorais.dev@gmail.com>
2022-11-30 22:02:27 -0300

    corrige enunciado do ex 4

--- fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/exercicio-funcoes-4.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/exercicio-funcoes-4.js
@@ -1,6 +1,8 @@
 /* Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.
 
-Array de teste: ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];. */
+Array de teste: ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];. 
+Valor esperado no retorno da função: Fernanda.
+*/
 
 function biggestName(array) {
     let biggest = ['', Number.NEGATIVE_INFINITY];
